791a0eaf4c683346981c04fb7e3ae48c8571c147
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-07-14 00:13:36 +0600

    number table

---
+++ tablep.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int sum(int a, int b);
+void printTable(int n);
+int main(){
+    int n;
+    printf("Enter Number :");
+    scanf("%d" , &n);
+
+    printTable(n);
+
+
+    return 0;
+}
+
+int sum(int x, int y){
+    return x + y;
+}
+
+void printTable(int n){
+    for(int i=1; i<=10; i++){
+        printf("%d\n" , i*n);
+    }
+}
